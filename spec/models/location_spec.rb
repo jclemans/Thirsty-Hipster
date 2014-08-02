97d40163a5248f4d1fd9d35b04779136548c3c63
Jeremy Clemans <jeremy@notch8.com>
2014-08-02 09:27:21 -0700

    adds test for HH start and end time formatting methods

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -9,4 +9,10 @@ describe Location do
   it { should validate_presence_of :end_time }
   it { should have_many :comments }
   it { should have_many :users }
+
+  it 'formats time into a human friendly format' do
+    location = Location.create(name: "Bob's Burgers", start_time: "16:00", end_time: "18:00")
+    location.hh_start_time.should eq "from 04:00PM"
+    location.hh_end_time.should eq "to 06:00PM"
+  end
 end
